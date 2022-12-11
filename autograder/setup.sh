dfec6b54d8bd15483ed4c481d2fbd6a130e34d22
Chia-Che Tsai <chiache@tamu.edu>
2022-12-11 17:00:55 -0600

    fix autograder scripts

--- autograder/setup.sh
+++ autograder/setup.sh
@@ -1,2 +1,5 @@
 apt update && apt install -y python python-pip
 pip install gradescope-utils>=0.3.1 boto3 google-cloud-storage azure-storage-blob
+
+cd /autograder/source
+git clone https://github.com/chiache/csce410_f22_proj4_tests -b autograder
