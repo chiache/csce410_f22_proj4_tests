2a418a6aca18ba89e10472743e158a6c657e9798
Chia-Che Tsai <chiache@tamu.edu>
2022-12-11 17:15:32 -0600

    fix test_uthread.py

--- test_uthread.py
+++ test_uthread.py
@@ -6,10 +6,61 @@ import time
 from gradescope_utils.autograder_utils.decorators import weight, visibility, number
 
 class test_uthread(unittest.TestCase):
-    @weight(10)
+    @weight(20)
     @number("4.1")
     def test1(self):
         p = subprocess.Popen(['./test1'], stdout=subprocess.PIPE)
         time.sleep(3)
         p.kill()
-        out, err = p.communicate()
+        out, _ = p.communicate()
+
+    @weight(15)
+    @number("4.2")
+    def test1(self):
+        p = subprocess.Popen(['./test2'], stdout=subprocess.PIPE)
+        time.sleep(3)
+        p.kill()
+        out, _ = p.communicate()
+
+    @weight(20)
+    @number("4.3")
+    def test1(self):
+        p = subprocess.Popen(['./test3'], stdout=subprocess.PIPE)
+        time.sleep(3)
+        p.kill()
+        out, _ = p.communicate()
+
+    @weight(10)
+    @number("4.3")
+    def test1(self):
+        p = subprocess.Popen(['./test6'], stdout=subprocess.PIPE)
+        time.sleep(3)
+        p.kill()
+        out, _ = p.communicate()
+
+
+    @weight(10)
+    @number("4.4.1")
+    def test1(self):
+        p = subprocess.Popen(['./test4'], stdout=subprocess.PIPE)
+        time.sleep(3)
+        p.kill()
+        out, _ = p.communicate()
+
+    @weight(10)
+    @number("4.4.1")
+    def test1(self):
+        p = subprocess.Popen(['./test5'], stdout=subprocess.PIPE)
+        time.sleep(3)
+        p.kill()
+        out, _ = p.communicate()
+
+    @weight(15)
+    @number("4.5")
+    def test1(self):
+        p = subprocess.Popen(['./test7'], stdout=subprocess.PIPE)
+        time.sleep(3)
+        p.kill()
+        out, _ = p.communicate()
+
+
